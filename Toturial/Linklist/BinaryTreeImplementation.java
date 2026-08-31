34202b899a910e630d421eb1dea606a4b8b19e0f
sachinsec <sachinsec@users.noreply.github.com>
2026-08-31 10:57:38 +0530

    binarystarting and practice

---
+++ Toturial/Linklist/BinaryTreeImplementation.java
@@ -0,0 +1,66 @@
+
+class Node{
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val){
+        this.val = val;
+    }
+}
+
+public class BinaryTreeImplementation {
+    public static void main(String[] args) {
+
+       /*
+                 3
+               /   \
+            4         2
+          /   \     /   \
+        -1     1   6     9
+       */
+        
+        Node a  = new Node(3); // root
+        Node b  = new Node(4);
+        Node c  = new Node(2);
+        Node d  = new Node(-1);
+        Node e  = new Node(1);
+        Node f  = new Node(6);
+        Node g  = new Node(9);
+
+
+        a.left = b;
+        a.right = c; 
+        
+        b.left = d;
+        b.right = e;
+
+        c.left = f; 
+        c.right = g;
+
+        display(a);
+        System.out.println();
+        System.out.println(size(a));
+
+    }
+
+    private static void display(Node root){
+        if (root ==null) {
+            return;
+        }
+        System.out.print(root.val+" ");
+        display(root.left);  // It is print all left side values
+        display(root.right); // It is print all right side values
+    }
+
+    private static int size(Node root){
+        if (root == null) {
+            return 0;            
+        }
+        int leftsize = size(root.left);
+        int rightsize = size(root.right);
+
+        return 1 + leftsize + rightsize; 
+    }
+    
+}
--- Toturial/Linklist/Practice.java
+++ Toturial/Linklist/Practice.java
@@ -1,83 +1,46 @@
-import java.util.Arrays;
 
-/**
- * Practice
- */
+class Node{
+    int value; 
+    Node right;
+    Node left;
+    Node root;
 
-class Queuecustom{
-    private int[] data;
-    static private int DEFAULT_SIZE = 10;
-    int end = 0;
-
-    public Queuecustom(){
-        this(DEFAULT_SIZE);
-    }
-    public Queuecustom(int size){
-       this.data = new int[size];
-    }
-
-    public boolean isFull(){
-        return end == data.length;
-    }
-
-    public boolean isEmpty(){
-        return end == 0;
-    }
-
-    public boolean insert(int val)throws Exception{
-
-        if (isFull()) {
-            throw new Exception("Queue is full");
-        }
-        data[end++]=val;
-        return true;
-    }
-
-    public int remove() throws Exception{
-        if (isEmpty()) {
-            throw new Exception("Queue is empty");
-        }
-        int removed = data[0];
-
-        for (int i = 1; i < end; i++) {
-            data[i-1] = data[i];
-    }
-        end --;
-        return removed;
-    }
-    public int Front()throws Exception{
-        if (isEmpty()) {
-            throw new Exception("queue is mepty");
-        }
-        return data[0];
-    }
-
-    public void Display()throws Exception{
-        if (isEmpty()){
-            throw new Exception("Queue is empty");
-        }
-            for (int i = 0; i < end; i++) {
-                System.out.println(data[i]);
-            }
+    public Node(int value){
+        this.value = value;
     }
 }
 public class Practice {
+    public static void main(String[] args) {
+        Node a = new Node(3);
+        Node b = new Node(2);
+        Node c = new Node(4);
+        Node d = new Node(1);
+        Node e = new Node(5);
+        Node f = new Node(4);
 
-    public static void main(String[] args)throws Exception {
-        Queuecustom qq = new Queuecustom();
-        qq.insert(10);
-        qq.insert(20);
-        qq.insert(40);
-        qq.insert(50);
-        qq.insert(60);
-        qq.insert(70);
-        qq.insert(80);
-        qq.insert(90);
-        qq.insert(100);
-        qq.insert(200);
-        qq.insert(300);
-        qq.insert(400);
+        a.left = b;a.right=c;
+        b.left = d; b.right=e;
+        c.left = f;
         
-        System.out.println(qq.isFull());
+        //display(a);
+        System.out.println(size(b));
+                
+    }
+
+    static void display(Node root){
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.value+" ");
+        display(root.left);
+        display(root.right);
+    }
+
+    static int size(Node root){
+        if(root == null) return 0;
+        int rootleft = size(root.left);
+        int rootright = size(root.right);
+
+        return 1 + rootleft + rootright;
     }
 }
